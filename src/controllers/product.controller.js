f94b0572406b4ddb67a4268d982b79bb412535cd
Erickferpinedo <erickfprogrammer@gmail.com>
2024-08-27 22:16:39 -0400

    basically done

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,89 +1,145 @@
-export async function getAllRecipes(req, res) {
-    try {
-      const recipes = await req.db.collection("recipes").find({}).toArray();
-      res.status(200).json(recipes);
-    } catch (error) {
-      res.status(500).json({ message: "Error fetching recipes", error });
-    }
-  }
-  
-  export async function createRecipe(req, res) {
-    try {
-      const { title, description, preparation, ingredients, nutritionalValues } =
-        req.body;
-  
-      if (!title || !description) {
-        return res
-          .status(400)
-          .json({ message: "Title and description are required" });
-      }
-  
-      // Crear el objeto de la receta con la 
-      const newRecipe = {
-        title,
-        description,
-        preparation: {
-          ingredients, 
-          cooking,
-          total,
-        },
-        ingredients: ingredients 
-          .split(",")
-          .map((ingredient) => ingredient.trim()), // Convertir a una lista de ingredientes
-        nutritionalValues: {
-          calories,
-          carbohydrates,
-          protein,
-          fat,
-        },
-      };
+import { ObjectId } from "mongodb";
 
-      const result = await req.db.collection("recipes").insertOne(newRecipe);
-  
-      // Retorna el objeto creado junto con el ID insertado
-      res.status(201).json({
-        _id: result.insertedId,
-        ...newRecipe,
+export async function getAllRecipes(req, res) {
+  try {
+    const recipes = await req.db.collection("recipes").find({}).toArray();
+    res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching recipes", error });
+  }
+}
+
+export async function getRecipeById(req, res) {
+  try {
+    const { id } = req.params;
+    const result = await req.db
+      .collection("recipes")
+      .findOne({ _id: new ObjectId(id) });
+
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching recipes", error });
+  }
+}
+
+export async function createRecipe(req, res) {
+  try {
+    const {
+      title,
+      description,
+      preparation,
+      instructions,
+      ingredients,
+      nutritionalValues,
+    } = req.body;
+
+    if (
+      !title ||
+      typeof title !== "string" ||
+      !description ||
+      typeof description !== "string"
+    ) {
+      return res.status(400).json({
+        message: "Title and description are required and type of string ",
       });
-    } catch (error) {
-      console.error("Error creating recipe:", error);
-      res.status(500).json({ message: "Error creating recipe", error });
     }
+
+    const newRecipe = {
+      title,
+      description,
+      preparation: {
+        ingredients: preparation.ingredients || "0 minutes",
+        cooking: preparation.cooking || "0 minutes",
+        total: preparation.total || "0 minutes",
+      },
+      instructions: instructions || [],
+      ingredients: Array.isArray(ingredients) ? ingredients : [],
+      nutritionalValues: {
+        calories: nutritionalValues.calories || 0,
+        carbohydrates: nutritionalValues.carbohydrates || "0g",
+        protein: nutritionalValues.protein || "0g",
+        fat: nutritionalValues.fat || "0g",
+      },
+    };
+
+    const result = await req.db.collection("recipes").insertOne(newRecipe);
+
+    res.status(201).json({
+      _id: result.insertedId,
+      ...newRecipe,
+    });
+  } catch (error) {
+    console.error("Error creating recipe:", error);
+    res.status(500).json({ message: "Error creating recipe", error });
   }
-  
-  export async function updateRecipe(req, res) {
-    try {
-      const { id } = req.params;
-      const updateData = req.body;
-  
-      const result = await req.db
-        .collection("recipes")
-        .updateOne({ _id: new ObjectId(id) }, { $set: updateData });
-  
-      if (result.matchedCount === 0) {
-        return res.status(404).json({ message: "Recipe not found" });
-      }
-  
-      res.status(200).json({ message: "Recipe updated successfully" });
-    } catch (error) {
-      res.status(500).json({ message: "Error updating recipe", error });
+}
+
+export async function updateRecipe(req, res) {
+  try {
+    const { id } = req.params;
+    const {
+      title,
+      description,
+      preparation,
+      instructions,
+      ingredients,
+      nutritionalValues,
+    } = req.body;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid recipe ID" });
     }
-  }
-  
-  export async function deleteRecipe(req, res) {
-    try {
-      const { id } = req.params;
-      const result = await req.db
-        .collection("recipes")
-        .deleteOne({ _id: new ObjectId(id) });
-  
-      if (result.deletedCount === 0) {
-        return res.status(404).json({ message: "Recipe not found" });
-      }
-  
-      res.status(200).json({ message: "Recipe deleted successfully" });
-    } catch (error) {
-      res.status(500).json({ message: "Error deleting recipe", error });
+
+    const updateData = {};
+    if (title) updateData.title = title;
+    if (description) updateData.description = description;
+    if (preparation) {
+      updateData.preparation = {
+        ingredients: preparation.ingredients || "0 minutes",
+        cooking: preparation.cooking || "0 minutes",
+        total: preparation.total || "0 minutes",
+      };
     }
+    if (instructions) updateData.instructions = instructions;
+    if (ingredients)
+      updateData.ingredients = Array.isArray(ingredients) ? ingredients : [];
+    if (nutritionalValues) {
+      updateData.nutritionalValues = {
+        calories: nutritionalValues.calories || 0,
+        carbohydrates: nutritionalValues.carbohydrates || "0g",
+        protein: nutritionalValues.protein || "0g",
+        fat: nutritionalValues.fat || "0g",
+      };
+    }
+
+    const result = await req.db
+      .collection("recipes")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    res.status(200).json({ message: "Recipe updated successfully" });
+  } catch (error) {
+    console.error("Error updating recipe:", error);
+    res.status(500).json({ message: "Error updating recipe", error });
   }
-  
+}
+
+export async function deleteRecipe(req, res) {
+  try {
+    const { id } = req.params;
+    const result = await req.db
+      .collection("recipes")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    res.status(200).json({ message: "Recipe deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting recipe", error });
+  }
+}
